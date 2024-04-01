c627f94539fa731505bca56d17994a9a334fa1e9
Zuhayer <zuhayer01@gmail.com>
2024-04-01 16:03:34 +0600

    first commit

--- Components/Loader.jsx
+++ Components/Loader.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { motion, AnimatePresence } from "framer-motion";
-import React, { useState } from "react";
+import { motion } from "framer-motion";
+import React from "react";
 
 const Loader = () => {
   return (
